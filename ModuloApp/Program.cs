fc5e70b478412b6128ed285dfe7b02e15dea7fb8
Arkadinio <a.kotlowski93@gmail.com>
2025-04-26 01:35:01 +0200

    Dodaj pliki projektów.

---
+++ ModuloApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę całkowitą: ");
+
+            var number = GetNumber();
+
+            typeNumber(number);
+        }
+
+        private static void typeNumber(int number)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Liczba jest Parzysta");
+            }
+            else
+            {
+                Console.WriteLine("Liczba jest Nieparzysta");
+            }
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int validateNumber) || validateNumber <= 0)
+                {
+                    Console.WriteLine("Błędna liczba");
+                    continue;
+
+                }
+
+                return validateNumber;
+            }
+
+        }
+    }
+}
